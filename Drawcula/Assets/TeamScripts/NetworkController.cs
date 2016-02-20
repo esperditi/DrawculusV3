e3ebe7d10d6435d800bbfd9d8864eb71384cb1c5
Miguel P <miguelp@iastate.edu>
2016-02-20 14:15:17 -0600

    Drawculus v3 project commit

---
+++ Drawcula/Assets/TeamScripts/NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkController : MonoBehaviour {
+
+	string _room = "DrawculaTest";
+
+	//On startup connect to photon network 
+	void Start () {
+		Debug.Log ("Connecting to Photon Server for Drawcula Project");
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+		Debug.Log ("Connection to Photon Server successful!");
+	}
+
+	//Join online cloud lobby and specified room
+	void OnJoinedLobby() {
+		Debug.Log ("Joined Lobby for Drawcula Project");
+		RoomOptions roomOptions = new RoomOptions() {};
+		PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+	}
+
+	//After connecting to room in lobby, instantiate the player
+	void OnJoinedRoom() {
+		PhotonNetwork.Instantiate ("NetworkedPlayer", Vector3.zero, Quaternion.identity, 0);
+	}
+}
---
+++ Drawcula/Assets/TeamScripts/NetworkedPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkedPlayer : Photon.MonoBehaviour {
+
+	public GameObject avatar;
+	public Transform playerGlobal;	//where the avatar is in space
+	public Transform playerLocal;	//avatar head movements
+
+	void Start () {
+		Debug.Log ("I'm instantiated");
+
+		//only if we're the player
+		if(photonView.isMine)
+		{
+			playerGlobal = GameObject.Find("FPSController").transform;
+			playerLocal = playerGlobal.Find ("FirstPersonCharacter");
+
+			//associate camera with avatar
+			this.transform.SetParent(playerLocal);
+
+			this.transform.localPosition = Vector3.zero;	//center the cube on the camera
+
+			//hide the avatar from ourselves (uncomment to toggle this)
+			//avatar.SetActive(false);
+		}
+	}
+
+	//Get new data and send to all players in room
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		//our player sending information out to players
+		if (stream.isWriting)
+		{
+			stream.SendNext(playerGlobal.position);
+			stream.SendNext(playerGlobal.rotation);
+			stream.SendNext(playerLocal.localPosition);
+			stream.SendNext(playerLocal.localRotation);
+		}
+		else    //our player recieving information from other players
+		{
+			this.transform.position = (Vector3)stream.ReceiveNext();
+			this.transform.rotation = (Quaternion)stream.ReceiveNext();
+			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
